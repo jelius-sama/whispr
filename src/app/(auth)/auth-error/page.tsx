bc831ecfa0ccb69ed8d09967ee8f11f266675cd2
Jelius <jelius.basumatary.sama@gmail.com>
2024-11-14 18:26:54 +0530

    Improvements

---
+++ src/app/(auth)/auth-error/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function AuthError() {
+    return (
+        <div>Something went wrong</div>
+    );
+}
--- src/app/api/(auth)/callback/route.ts
+++ src/app/api/(auth)/callback/route.ts
@@ -2,17 +2,31 @@ import { createServerClient } from '@/server/supabase/edge';
 import { ServerRuntime } from 'next';
 import { NextResponse } from 'next/server';
 
-export const runtime: ServerRuntime = 'edge';
+export const runtime: ServerRuntime = "edge";
 
 export async function GET(request: Request) {
-    const requestUrl = new URL(request.url);
-    const code = requestUrl.searchParams.get('code');
+    const { searchParams, origin } = new URL(request.url);
+    const code = searchParams.get('code');
+    // if "next" is in param, use it as the redirect URL
+    const next = searchParams.get('next') ?? '/';
 
     if (code) {
         const supabase = createServerClient();
-        await supabase.auth.exchangeCodeForSession(code);
+        const { error } = await supabase.auth.exchangeCodeForSession(code);
+        if (!error) {
+            const forwardedHost = request.headers.get('x-forwarded-host'); // original origin before load balancer
+            const isLocalEnv = process.env.NODE_ENV === 'development';
+            if (isLocalEnv) {
+                // we can be sure that there is no load balancer in between, so no need to watch for X-Forwarded-Host
+                return NextResponse.redirect(`${origin}${next}`);
+            } else if (forwardedHost) {
+                return NextResponse.redirect(`https://${forwardedHost}${next}`);
+            } else {
+                return NextResponse.redirect(`${origin}${next}`);
+            }
+        }
     }
 
-    // URL to redirect to after sign in process completes
-    return NextResponse.redirect(requestUrl.origin);
+    // return the user to an error page with instructions
+    return NextResponse.redirect(`${origin}/auth-error`);
 }
